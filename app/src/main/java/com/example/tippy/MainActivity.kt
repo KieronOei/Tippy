8906861607f9c274e55fe3e2e7d681397d222b5c
KieronOei <65146443+KieronOei@users.noreply.github.com>
2022-03-08 22:34:08 +0800

    Added Split by N people function

--- app/src/main/java/com/example/tippy/MainActivity.kt
+++ app/src/main/java/com/example/tippy/MainActivity.kt
@@ -15,21 +15,26 @@ private const val TAG = "MainActivity"
 private const val INITIAL_TIP_PERCENT = 15
 class MainActivity : AppCompatActivity() {
     private lateinit var etBaseAmount: EditText
+    private lateinit var etSplitNumber: EditText
     private lateinit var seekBarTip: SeekBar
     private lateinit var tvTipPercentLabel: TextView
     private lateinit var tvTipAmount: TextView
     private lateinit var tvTotalAmount: TextView
     private lateinit var tvTipDescription: TextView
+    private lateinit var tvIndividualAmount: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         etBaseAmount = findViewById(R.id.etBaseAmount)
+        etSplitNumber = findViewById(R.id.etSplitNumber)
         seekBarTip = findViewById(R.id.seekBarTip)
         tvTipPercentLabel = findViewById(R.id.tvTipPercentLabel)
         tvTipAmount = findViewById(R.id.tvTipAmount)
         tvTotalAmount = findViewById(R.id.tvTotalAmount)
         tvTipDescription = findViewById(R.id.tvTipDescription)
+        tvIndividualAmount = findViewById(R.id.tvIndividualAmount)
 
         // Initial setting of values
         seekBarTip.progress = INITIAL_TIP_PERCENT
@@ -41,6 +46,7 @@ class MainActivity : AppCompatActivity() {
                 tvTipPercentLabel.text = "$p1%"
                 computeTipAndTotal()
                 updateTipDescription(p1)
+                computeIndividual()
             }
 
             override fun onStartTrackingTouch(p0: SeekBar?) {
@@ -62,13 +68,40 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun afterTextChanged(p0: Editable?) {
-                Log.i(TAG, "afterTextChanged $p0")
+                Log.i(TAG, "afterTextChanged_etBaseAmount $p0")
                 computeTipAndTotal()
             }
 
         })
 
+        etSplitNumber.addTextChangedListener(object:TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                Log.i(TAG,"afterTextChanged_etSplitNumber $p0")
+                computeIndividual()
+            }
+
+        })
+
+
+    }
+
+    private fun computeIndividual() {
+        if (etSplitNumber.text.isEmpty()) {
+            tvIndividualAmount.text = ""
+            return // this if statement is to ensure the rest of the code below is not run if you backspace all the numbers
+        }
+        val totalAmount = tvTotalAmount.text.toString().toDouble()
+        val numPeople = etSplitNumber.text.toString().toInt()
+        val individualAmount = totalAmount / numPeople
+        tvIndividualAmount.text = "%.2f".format(individualAmount)
     }
 
     private fun updateTipDescription(tipPercent: Int) {
@@ -108,7 +141,7 @@ class MainActivity : AppCompatActivity() {
 }
 
 // Extensions idea
-// 1. Split the bill by N people
+// 1. Split the bill by N people [DONE]
 // 2. Round the final amount up/down which should update the tip amount and percent automatically
 // 3. Design/color updates
 // 4. Replace the text describing the tip ("poor","good",etc) with emojis
